f051beb91ca70cc962e0231bfe58f1ef58cbda8b
allen.chang <allen.chang@allpay.com.tw>
2017-09-20 15:53:01 +0800

    Node.js SDK 已更新到新的網址,請至https://github.com/O-pay 取得,感謝

--- Invoice/opay_invoice_nodejs/lib/opay_invoice/version.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ying.wu on 2017/6/12.
- */
-class OPayInvoiceVersion{
-    constructor(){
-        this.version = '1.0.0';
-    }
-}
-module.exports = OPayInvoiceVersion;
--- Payment/opay_payment_nodejs/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by ying.wu on 2017/6/29.
- */
-'use strict';
-
-module.exports = require('./lib/opay_payment.js');
